1cbaae2e4bf535790386247e53f226797e56641c
Natalya1984 <lisa.volma1984@gmail.com>
2022-01-17 23:31:54 +0300

    17.01

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='app-wrapper'>
+    <header className='header'>
+        <img src ='https://cdn.pixabay.com/photo/2017/12/13/23/27/no-background-3017971_960_720.png' />
+    </header>
+        <nav className='nav'>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>Main content</div>
+        </nav>
+            <div className='content'>
+                main content
+            </div>
+
+
     </div>
   );
 }
+
+
 
 export default App;
